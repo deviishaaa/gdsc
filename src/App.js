f162e64a9c6969538cbf00c00e9512b29c4c7ab0
Devisha Gupta <devisha18.003gupta@gmail.com>
2023-03-24 16:22:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+        <p>
+          Devisha- E22CSEU0633 
+        </p>
+        
+      
+    </div>
+  );
+}
+
+export default App;
